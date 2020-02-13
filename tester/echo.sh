4491d753a7aed71ca1a7d1f1aa6b87599f5b5463
Thomas Claudel <contact@tclaudel.fr>
2020-02-13 11:10:31 +0100

    test improved

--- tester/echo.sh
+++ tester/echo.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
+
 echo 'echo pouet'
-sleep 1.2
+sleep ""$1""
 
 echo 'echo "pouet'
-sleep 0.5
+sleep 0.1
 echo '"'
-sleep 1.2
+sleep "$1"
 
 echo 'echo "pouet"str""'
-sleep 1.2
+sleep "$1"
 
 echo 'echo "po  uet"str""'
-sleep 1.2
+sleep "$1"
 
 echo 'echo euh NTM scialpi'
-sleep 1.2
+sleep "$1"
 
 echo 'echo "pouet" "str"'
-sleep 1.2
+sleep "$1"
